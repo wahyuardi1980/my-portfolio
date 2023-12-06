15fefec35c621be378b46391e078967fd6f3db26
rizkihidayat2000 <rizkyhidayatoffice@gmail.com>
2023-12-07 02:37:17 +0700

    Finishing

---
+++ js/script.js
@@ -0,0 +1,9 @@
+var nav = document.querySelector("nav");
+
+window.addEventListener("scroll", () => {
+  if (document.documentElement.scrollTop > 20) {
+    nav.classList.add("sticky");
+  } else {
+    nav.classList.remove("sticky");
+  }
+});
