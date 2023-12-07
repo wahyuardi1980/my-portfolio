303e2cd5da3d1e0ae44a83ca4e500c65a6e3f1b8
rizkihidayat2000 <rizkyhidayatoffice@gmail.com>
2023-12-07 20:15:36 +0700

    Finishing Akhir

--- js/script.js
+++ js/script.js
@@ -1,9 +1,25 @@
 var nav = document.querySelector("nav");
+var gettoTop = document.getElementById("tombol-atas");
 
+// function navbar sticky-top
 window.addEventListener("scroll", () => {
   if (document.documentElement.scrollTop > 20) {
     nav.classList.add("sticky");
   } else {
     nav.classList.remove("sticky");
   }
+});
+
+// function scroll to top
+window.addEventListener("scroll", () => {
+  if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
+    gettoTop.style.display = "block";
+  } else {
+    gettoTop.style.display = "none";
+  }
+});
+
+gettoTop.addEventListener("click", function () {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
 });
